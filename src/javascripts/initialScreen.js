359942df212525ddecb25624681bdd5ee54ee681
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-25 16:39:17 -0600

    added buttons, no functionality yet

--- src/javascripts/initialScreen.js
+++ src/javascripts/initialScreen.js
@@ -1,12 +1,20 @@
-
 import renderToDom from './helpers.js/renderToDom';
 
 const initialScreen = () => {
   let domString = '';
   domString += `
-  `;
+    <div id="btn-container></div>
+      <div id="top">
+        <div><button type="button" id="play-btn">PLAY</button></div>
+      </div>
 
-  renderToDom('body', domString);
+      <div id="bottom">
+        <div><button type="button" id="eat-btn">EAT</button></div>
+        <div><button type="button" id="fight-btn">FIGHT</button></div>
+        <div><button type="button" id="sleep-btn">SLEEP</button></div>
+    </div>`;
+
+  renderToDom('#app', domString);
 };
 
 export default initialScreen;
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,8 @@
 import '../styles/main.scss';
+import initialScreen from './initialScreen';
 
 const init = () => {
-
+  initialScreen();
 };
 
 init();
