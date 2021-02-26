5560080fb48a37e492a269530304602893c9351e
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-26 17:36:33 -0600

    eat functions working

--- src/javascripts/eat.js
+++ src/javascripts/eat.js
@@ -2,12 +2,17 @@ import renderToDom from './helpers/renderToDom';
 
 let full = 100;
 
+const updateFullnessScore = () => {
+  document.querySelector('#fullness-value').innerHTML = `${full}`;
+};
+
 const feedHealthyFood = (e) => {
   e.preventDefault();
   full += 10;
   if (full > 100) {
     full = 100;
   }
+  updateFullnessScore();
 };
 
 const feedUnHealthyFood = (e) => {
@@ -16,13 +21,14 @@ const feedUnHealthyFood = (e) => {
   if (full <= 0) {
     full = 'death';
   }
+  updateFullnessScore();
 };
 
 const eatCard = () => {
   let domString = '';
   domString += `
     <div id="eat-name"><h3>Name: EAT</h3></div>
-    <div id="fullness-score">Fullness Score: <b>${full}</b></div>
+    <div id="fullness-score">Fullness Score: <b><span id="fullness-value">${full}</span></b></div>
     <div id="eat-btn-container">
       <button type="button" class="btn btn-light" id="eat-healthy-btn">Healthy</button>
       <button type="button" class="btn btn-dark" id="eat-unhealthy-btn">Unhealthy</button>
