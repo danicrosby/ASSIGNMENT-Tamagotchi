0377e92f0dcd171ba80f832160cb3d2769ec4258
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-25 19:35:18 -0600

    quadrants made with flex-box, need to style

--- src/javascripts/initialScreen.js
+++ src/javascripts/initialScreen.js
@@ -3,18 +3,30 @@ import renderToDom from './helpers.js/renderToDom';
 const initialScreen = () => {
   let domString = '';
   domString += `
-    <div id="btn-container></div>
-      <div id="top">
-        <div><button type="button" id="play-btn">PLAY</button></div>
-      </div>
-
-      <div id="bottom">
-        <div><button type="button" id="eat-btn">EAT</button></div>
-        <div><button type="button" id="fight-btn">FIGHT</button></div>
-        <div><button type="button" id="sleep-btn">SLEEP</button></div>
-    </div>`;
+  <div id="quad-container">
+  
+    <div id="left-row">
+      <div id="play" class="quad">PLAY</div>
+      <div id="eat" class="quad">EAT</div>
+    </div>
+    <div id="right-row">
+      <div id="fight" class="quad">FIGHT</div>
+      <div id="sleep" class="quad">SLEEP</div>
+    </div>
+  </div>`;
 
   renderToDom('#app', domString);
 };
 
 export default initialScreen;
+
+// <div id="btn-container></div>
+// <div id="top">
+//   <div><button type="button" id="play-btn">PLAY</button></div>
+// </div>
+
+// <div id="bottom">
+//   <button type="button" id="eat-btn"> EAT </button>
+//   <button type="button" id="fight-btn">FIGHT</button>
+//   <button type="button" id="sleep-btn">SLEEP</button>
+// </div>
