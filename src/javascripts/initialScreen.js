42c888f422672a1c0beb1052c488ec4d60615bc7
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-25 21:09:04 -0600

    quads and button placement almost done, need to more play

--- src/javascripts/initialScreen.js
+++ src/javascripts/initialScreen.js
@@ -3,30 +3,35 @@ import renderToDom from './helpers.js/renderToDom';
 const initialScreen = () => {
   let domString = '';
   domString += `
-  <div id="quad-container">
-  
+    <div id="quad-container">
+
     <div id="left-row">
-      <div id="play" class="quad">PLAY</div>
-      <div id="eat" class="quad">EAT</div>
+      <div id="eat" class="quad">EAT VAR FULL</div>
+      <div id="fight" class="quad">FIGHT VAR STRENGTH</div>
     </div>
+
+    <div id="btn-container">
+
+    <div id="top">
+      <button type="button" id="play-btn">PLAY</button>
+    </div>
+
+    <div id="bottom">
+      <button type="button" id="eat-btn"> EAT </button>
+      <button type="button" id="fight-btn">FIGHT</button>
+      <button type="button" id="sleep-btn">SLEEP</button>
+    </div>
+
+    </div>
+
     <div id="right-row">
-      <div id="fight" class="quad">FIGHT</div>
-      <div id="sleep" class="quad">SLEEP</div>
+      <div id="play" class="quad">PLAY VAR FUN</div>
+      <div id="sleep" class="quad">SLEEP VAR ENERGY</div>
     </div>
+
   </div>`;
 
   renderToDom('#app', domString);
 };
 
 export default initialScreen;
-
-// <div id="btn-container></div>
-// <div id="top">
-//   <div><button type="button" id="play-btn">PLAY</button></div>
-// </div>
-
-// <div id="bottom">
-//   <button type="button" id="eat-btn"> EAT </button>
-//   <button type="button" id="fight-btn">FIGHT</button>
-//   <button type="button" id="sleep-btn">SLEEP</button>
-// </div>
